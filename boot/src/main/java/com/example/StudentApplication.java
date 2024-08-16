b1cb8e189b0945d95fd5c478d411eae64fed538b
m83city <rostorguievv@gmail.com>
2024-08-16 13:50:22 +0300

    rewrite main method

---
+++ boot/src/main/java/com/example/StudentApplication.java
@@ -0,0 +1,10 @@
+package com.example;
+
+
+import org.springframework.boot.SpringApplication;
+
+public class StudentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentApplication.class);
+    }
+}
